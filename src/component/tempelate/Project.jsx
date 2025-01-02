f44649f8d18c138b07881b0f6ae059631a15f359
SachinRuhela1922 <156918990+SachinRuhela1922@users.noreply.github.com>
2025-01-01 23:08:28 -0800

    Update Project.jsx

--- src/component/tempelate/Project.jsx
+++ src/component/tempelate/Project.jsx
@@ -10,44 +10,44 @@ const Project = () => {
     return (
         <div className={ProjectCSS.body}>
             <p className={ProjectCSS.para}>#Creative and impressive projects</p>
-              <h1 className={ProjectCSS.heading}>Our Featured Products</h1>
+              <h1 className={ProjectCSS.heading}>Our Featured Projects</h1>
             <div className={ProjectCSS.cardcontainer}>
 
                 <div className={ProjectCSS.card}>
                     <img className={ProjectCSS.img} src={image1} alt="Product 1" />
-                    <h3 className={ProjectCSS.h3}>Product 1</h3>
-                    <p className={ProjectCSS.p}>Some description for product 1.</p>
-                    <button className={ProjectCSS.btn}>Buy Now</button>
+                    <h3 className={ProjectCSS.h3}>Ultron</h3>
+                    <p className={ProjectCSS.p}>This Project is based on IOT based Application </p>
+                    <button className={ProjectCSS.btn}>Redirect Now</button>
                 </div>
 
                 <div className={ProjectCSS.card}>
                     <img className={ProjectCSS.img} src={image2} alt="Product 2" />
-                    <h3 className={ProjectCSS.h3}>Product 2</h3>
-                    <p className={ProjectCSS.p}>Some description for product 2.</p>
-                    <button className={ProjectCSS.btn}>Buy Now</button>
+                    <h3 className={ProjectCSS.h3}>Medoc</h3>
+                    <p className={ProjectCSS.p}>This project based on Web Application </p>
+                    <button className={ProjectCSS.btn}>Redirect Now</button>
                 </div>
 
                 <div className={ProjectCSS.card}>
                     <img className={ProjectCSS.img} src={image3} alt="Product 3" />
-                    <h3 className={ProjectCSS.h3}>Product 3</h3>
-                    <p className={ProjectCSS.p}>Some description for product 3.</p>
-                    <button className={ProjectCSS.btn}>Buy Now</button>
+                    <h3 className={ProjectCSS.h3}>Maxton.X</h3>
+                    <p className={ProjectCSS.p}>This project based on Game development </p>
+                    <button className={ProjectCSS.btn}>Redirect  Now</button>
                 </div>
                 <div className={ProjectCSS.card}>
                     <img className={ProjectCSS.img} src={image4} alt="Product 3" />
-                    <h3 className={ProjectCSS.h3}>Product 3</h3>
-                    <p className={ProjectCSS.p}>Some description for product 3.</p>
-                    <button className={ProjectCSS.btn}>Buy Now</button>
+                    <h3 className={ProjectCSS.h3}>Holomad</h3>
+                    <p className={ProjectCSS.p}>This project based on Full Stack Application </p>
+                    <button className={ProjectCSS.btn}>Redirect Now</button>
                 </div>
                 <div className={ProjectCSS.card}>
                     <img className={ProjectCSS.img} src={image5} alt="Product 3" />
-                    <h3 className={ProjectCSS.h3}>Product 3</h3>
-                    <p className={ProjectCSS.p}>Some description for product 3.</p>
-                    <button className={ProjectCSS.btn}>Buy Now</button>
+                    <h3 className={ProjectCSS.h3}>Mellow</h3>
+                    <p className={ProjectCSS.p}>This project based on Front-end Only</p>
+                    <button className={ProjectCSS.btn}>Redirect Now</button>
                 </div>
               
             </div>
-            <button className={ProjectCSS.more}>More..</button>
+            <button className={ProjectCSS.more}><Link   to='/project'>More Projects</Link></button>
         </div>
     )
 }
